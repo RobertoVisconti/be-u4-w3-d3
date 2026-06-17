1835ae526ba780c24e56a68f2d0e72d02dfb5a27
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-17 16:47:45 +0200

    fix

--- src/main/java/robertovisconti/exceptions/NotFoundByIdException.java
+++ src/main/java/robertovisconti/exceptions/NotFoundByIdException.java
@@ -2,6 +2,6 @@ package robertovisconti.exceptions;
 
 public class NotFoundByIdException extends RuntimeException {
     public NotFoundByIdException(String id) {
-        super("Il record con id " + id + " non è stato trovato.");
+        super("Il record con id: " + id + " non è stato trovato.");
     }
 }
